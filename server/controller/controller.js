1e9b09eec6c122ebf58e7a9492e26dd7d59ce0b9
shamsursajib <shamsur.sajib@outlook.com>
2022-01-10 14:50:13 +0600

    Task On Controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,19 +1,54 @@
+const req = require('express/lib/request');
 var Userdb = require('../model/model');
 
 //create & save new user
 
 exports.create = (req, res) => {
     //validate a request 
-    // if(!req.body){
-    //     res.status(400).send
-    // }
+    if (!req.body) {
+        res.status(400).send({ message: "Content Cannot Be empty" });
+        return;
+    }
+    //new user 
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    //save user in db
+
+    user
+        .save(user)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occoured"
+            });
+        });
+
+
 }
+
+
+
+
 
 //retive & retuen user / sinle user 
 
 
 exports.find = (req, res) => {
-    //
+    Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message || "Error  Occured" })
+        })
+
 
 }
 
@@ -21,6 +56,23 @@ exports.find = (req, res) => {
 // update a new identied user by user id 
 
 exports.update = (req, res) => {
+    if (req.body) {
+        return res.status(400)
+            .send({ message: " Data Cannot be empty" })
+    }
+    const id = req.param.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot update user with ${id}. Maybe user Not found` })
+            } else {
+                res.send(data)
+            }
+        })
+
+        .catch(err => {
+            res.status(500).send({ message: " Error Update user Information" })
+        })
 
 }
 
